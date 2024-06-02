a0b439c5a58583420befcc327e38ab1712c8b43f
Omar Hassanein <omar.hassanein@1und1.de>
2024-06-02 15:49:25 +0200

    Update camera permission to handle different scenarios and refactor code to follow the same code signature

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
@@ -2,11 +2,12 @@ package com.jetbrains.kmpapp
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.navigator.Navigator
+import com.jetbrains.kmpapp.screens.camera_permission.CameraPermissionScreen
 
 @Composable
 fun App() {
     MaterialTheme {
-//        Navigator(ListScreen)
-        CameraScreen()
+        Navigator(CameraPermissionScreen())
     }
 }
